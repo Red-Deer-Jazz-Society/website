a76dc2ba441a5894465b05328504f20f4d46522e
Ayrton Chilibeck <git@blobfish.icu>
2025-01-22 14:31:56 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Red Deer Jazz Society Website
+
+A website to keep track of
+- upcoming events
+- contact information for the society
+- information about the society
+- etc.
+
+
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs=[
+	    pkgs.nodejs_20
+	    pkgs.nodePackages.gatsby-cli
+	    pkgs.nodePackages.npm
+            pkgs.nodePackages.pnpm
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+          ];
+        };
+      }
+    );
+}
